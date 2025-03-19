b0544096df8893cc73e667a5bc0783eb18871fb8
NimraRasheed37 <binteshaheen.na@gmail.com>
2025-03-19 06:24:56 +0500

    1st commit

---
+++ library_manager.py
@@ -0,0 +1,16 @@
+# personal library project
+
+# store book as a dictionary with details like:
+    # title, author, publication year, genre, and read status.
+
+
+# menu system: 
+    # Add a book
+    # Remove a book
+    # Search for a book
+    # Display all books
+    # Display statistics (total books, percentage read)
+    # Exit
+
+# basic stratistics
+# save and load library
